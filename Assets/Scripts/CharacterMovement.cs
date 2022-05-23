1701d8a44f316173e769ae45a86bb71f2b39df6e
MAO\Charles <zhaoyanmao@gmail.com>
2022-05-22 21:56:15 -0700

    added test scene

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -17,27 +17,30 @@ public class CharacterMovement : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _rb.constraints = RigidbodyConstraints.FreezePosition;
     }
 
     public void ShootPlayer()
     {
         _rb.AddForce(transform.forward * _shootingForce, ForceMode.Impulse);
+        _rb.constraints = RigidbodyConstraints.None;
     }
 
     private void FixedUpdate()
     {
-        Vector3 LookDirection = new Vector3(_moveInput.x, 0f, _moveInput.y).normalized;
-        float horturnDirection = -Mathf.Sign(Vector3.Cross(LookDirection, transform.forward).y);
-        float turnMagnitude1 = 1f - Mathf.Clamp01(Vector3.Dot(transform.forward, LookDirection));
-        float vertTemp = Vector3.Dot(Vector3.Cross(LookDirection, transform.forward), transform.right);
-        float verturnDirection = -Mathf.Sign(vertTemp);
-        float turnMagnitude2 = 1f - Mathf.Clamp01(vertTemp);
-        if (_moveInput.magnitude < 0.1f) { turnMagnitude1 = 0f;
-            turnMagnitude2 = 0f;
-        }
-        float angularVelocity1 = turnMagnitude1 * _turnSpeed * horturnDirection;
-        Vector3 angularVelocity2 = transform.right * turnMagnitude2 * _turnSpeed * verturnDirection;
-        angularVelocity2.y += angularVelocity1;
-        _rb.angularVelocity = angularVelocity2;
+        //Vector3 LookDirection = new Vector3(_moveInput.x, 0f, _moveInput.y).normalized;
+        //float horturnDirection = -Mathf.Sign(_moveInput.x);
+        //float turnMagnitude1 = 1f - Mathf.Clamp01(Vector3.Dot(transform.forward, LookDirection));
+        //float vertTemp = Vector3.Dot(Vector3.Cross(LookDirection, transform.forward), transform.right);
+        //float verturnDirection = -Mathf.Sign(vertTemp);
+        //float turnMagnitude2 = 1f - Mathf.Clamp01(vertTemp);
+        //if (_moveInput.magnitude < 0.1f) { turnMagnitude1 = 0f;
+        //    turnMagnitude2 = 0f;
+        //}
+        //float angularVelocity1 = turnMagnitude1 * _turnSpeed * horturnDirection;
+        //Vector3 angularVelocity2 = transform.right * turnMagnitude2 * _turnSpeed * verturnDirection;
+        //angularVelocity2.y += angularVelocity1;
+        _rb.angularVelocity = (_moveInput.x * transform.up + _moveInput.y * transform.right) * _turnSpeed;
+        //transform.rotation *= Quaternion.LookRotation((_moveInput.x * transform.up + _moveInput.y * transform.right) * _turnSpeed);
     }
 }
